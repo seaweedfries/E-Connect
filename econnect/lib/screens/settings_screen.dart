1912c677d0e0754965bcb8b548855535b3af6b5d
seaweedfries <yeowanli24@gmail.com>
2022-07-16 17:41:03 +0800

    feat: settings page

--- econnect/lib/screens/settings_screen.dart
+++ econnect/lib/screens/settings_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/material.dart';
 import 'package:econnect/constants.dart';
+import 'package:econnect/widgets/setting_item.dart';
 
 
 class SettingsScreen extends StatefulWidget {
@@ -30,10 +31,52 @@ class _SettingsScreenState extends State<SettingsScreen> {
                   image: AssetImage('./images/user.jpg'),
                 ).image,
               ),
-            ),)
-        ]
-
-      )
-      );
+            ),
+            title: const Text(
+              'carbohydrate',
+              style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.w500),
+            ),
+            subtitle: const Text(
+              'hehe hci',
+              style: TextStyle(fontSize: 13.0),
+            ),
+          ),
+          const Divider(height: 0.0,),
+          SettingItem(
+            icon: Icons.vpn_key,
+            title: 'Account',
+            subtitle: 'Privacy, security, change number',
+          ),
+          SettingItem(
+            icon: Icons.chat,
+            title: 'Chats',
+            subtitle: 'Theme, wallpapers, chat history',
+          ),
+          SettingItem(
+            icon: Icons.notifications,
+            title: 'Notifications',
+            subtitle: 'Message, group & call tones',
+          ),
+          SettingItem(
+            icon: Icons.data_usage_rounded,
+            title: 'Storage and data',
+            subtitle: 'Network usage, auto-download',
+          ),
+          SettingItem(
+            icon: Icons.help_outline_outlined,
+            title: 'Help',
+            subtitle: 'Help centre, contact us, privacy policy',
+          ),
+          SettingItem(
+            icon: Icons.group,
+            title: 'Invite a friend',
+          ),
+          SettingItem(
+            icon: Icons.image_not_supported,
+            title: 'Silver mode',
+          ),
+        ],
+      ),
+    );
   }
 }
---
+++ econnect/lib/widgets/setting_item.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:econnect/constants.dart';
+
+class SettingItem extends StatelessWidget {
+
+  final IconData icon;
+  final String title;
+  final String subtitle;
+  final padding;
+
+  SettingItem({this.icon = Icons.error, this.title = 'Error', this.subtitle = '', this.padding});
+
+  @override
+  Widget build(BuildContext context) {
+    if(subtitle == null) {
+      return ListTile(
+        contentPadding: padding ?? const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+        leading: icon == null ? null : Container(
+          padding: const EdgeInsets.all(8.0),
+          child: Icon(
+            icon,
+            color: CustomColors.kPrimaryColor,
+          ),
+        ),
+        title: Text(
+          title,
+          style: const TextStyle(
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+      );
+    }
+    return ListTile(
+      contentPadding: padding ?? null,
+      leading: icon == null ? null : Container(
+        padding: const EdgeInsets.all(8.0),
+        child: Icon(
+          icon,
+          color: CustomColors.kPrimaryColor,
+        ),
+      ),
+      title: Text(
+        title,
+        style: const TextStyle(
+          fontWeight: FontWeight.w500,
+        ),
+      ),
+      subtitle: Text(
+          subtitle
+      ),
+    );
+  }
+
+}
