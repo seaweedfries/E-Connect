0fcc6205953e6c4a0d586f78a8ff52dd3034b529
seaweedfries <yeowanli24@gmail.com>
2022-07-26 01:23:45 +0800

    fix: merge conflicts

--- econnect/lib/tabs/calls_tab.dart
+++ econnect/lib/tabs/calls_tab.dart
@@ -94,9 +94,6 @@ class DetailCallScreen extends StatelessWidget {
                   //Callingscreen();
                 },
               ),
-              title: const Text(
-                'Bob',
-              ),
             ],
           )
         ),
