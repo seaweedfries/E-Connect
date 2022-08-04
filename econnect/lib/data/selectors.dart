1358c03850a0f90cc3e66a8212076e18ee298758
Claudiapotato <93199737+Claudiapotato@users.noreply.github.com>
2022-08-04 19:26:03 +0800

    updated selectors dart

--- econnect/lib/data/selectors.dart
+++ econnect/lib/data/selectors.dart
@@ -1,13 +1,81 @@
 final input = {
   //help function
   'help': ['How can I help?'],
-  'i want to make a call': ['Sure, I can explain step by step!', 'First, go back to the main page by clicking on the arrow at the top left of the page', 'Then click on the "Calls" tab to go to the Calls section.', 'After that, you can either click on the contact history to make a call,', 'Or click on the floating call button on the bottom right of the screen to look for your contact.'],
+  'i want to make a call': [
+    'Sure, I can explain step by step!',
+    'First, go back to the main page by clicking on the arrow at the top left of the page',
+    'Then click on the "Calls" tab to go to the Calls section.',
+    'After that, you can either click on the contact history to make a call,',
+    'Or click on the floating call button on the bottom right of the screen to look for your contact.'
+  ],
+  'i want to send a message': [
+    'Sure, I can explain step by step!',
+    'First, go back to the main page by clicking on the arrow at the top left of the page',
+    'Then click on the "Chats" tab to go to the Messages section.',
+    'After that, you can choose who you want to message'
+        'And click on the contact history to send him/her message,'
+  ],
+
+  '打电话': [
+    '确定，我可以解释步骤！',
+    '首先，点击页面左上角的箭头，然后点击“通话”选项卡，',
+    '然后，你可以点击通话记录，或者点击屏幕右下角的悬浮按钮，查找你想要联系的人。'
+  ],
+
+  '发消息': [
+    '确定，我可以解释步骤！',
+    '首先，点击页面左上角的箭头，然后点击“聊天”选项卡，',
+    '然后，你可以点击聊天记录，或者点击屏幕右下角的悬浮按钮，查找你想要联系的人。'
+  ],
 
   //settings function
-  'settings': ['What settings would you like to change?', 'You can', 'Turn Helpbot off'],  
+  'settings': [
+    'To change the settings, go back to the main page by clicking on the arrow at the top left of the page',
+    'Then click on the 3 vertical dots button at the top right corner to go to the Settings section.',
+    'After that, you can change the settings of the app.',
+    'Try to toggle between the elderly on and off modes by clicking on the Silver Mode button'
+  ],
+  '设置': [
+    '要更改设置，点击页面左上角的箭头，然后点击“设置”选顢卡，',
+    '然后，你可以点击三角形按钮，进入设置页面。',
+    '然后，你可以更改应用的设置。',
+    '试着点击银色模式按钮，切换到老人模式。'
+  ],
   'turn off': 'You can turn me on by telling me "Turn on". Goodbye!',
 
   //general
-  'suggest something': ['Based on your past interests with your contacts,', 'Here is a suggested video for you to watch', 'Title: How and where to find your thermos bottle', 'URL: https://www.youtube.com/watch?v=dQw4w9WgXcQ', 'Would you like to watch this video?'],
-  'recommendations': ['Based on your past activities with your contacts,', 'Would you like to participate in this activity?', 'Activity Title: Tampinese Walk for Thermos', 'Time: Sunday 25th of July 2022', 'Location: Tampinese Walk', 'URL: https://www.facebook.com/events/89841008766008/', 'Would you like to join this activity?'],
+  'suggest something': [
+    'Based on your past interests with your contacts,',
+    'Here is a suggested video for you to watch',
+    'Title: How and where to find your thermos bottle',
+    'URL: https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+    'Would you like to watch this video?'
+  ],
+  '提出一些建议': [
+    '根据你与你的联系人的历史兴趣，',
+    '这里是一个为你推荐的视频',
+    '标题 : 如何找到你的热水瓶',
+    '链接 : https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+    '你想要观看这个视频吗？'
+  ],
+
+  'recommendations': [
+    'Based on your past activities with your contacts,',
+    'Would you like to participate in this activity?',
+    'Activity Title: Tampinese Walk for Thermos',
+    'Time: Sunday 25th of July 2022',
+    'Location: Tampinese Walk',
+    'URL: https://www.facebook.com/events/89841008766008/',
+    'Would you like to join this activity?'
+  ],
+
+  '建议': [
+    '根据你与你的联系人的历史活动，',
+    '你想要参加这个活动吗？',
+    '活动标题 : 塔比森游行',
+    '时间 : 周日 25 号 2022',
+    '地点 : 塔比森游行',
+    '链接 : https://www.facebook.com/events/89841008766008/',
+    '你想要参加这个活动吗？'
+  ],
 };
